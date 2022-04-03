bf07246918178483768d12a30ba273030951a6d7
Erisa A <erisa@erisa.uk>
2022-04-03 08:02:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,4 @@
 git config --global commit.gpgsign true
 
 # node 16 > all
-nvm install 16
-nvm use --delete-prefix v16.14.2
-
-# need to move away from globals at some point.. 
-yarn global add serve @cloudflare/wrangler miniflare @railway/cli
+echo "lts/*" > $HOME/.nvmrc 
